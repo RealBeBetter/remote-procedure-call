1ebce756737ffada678b28e1b78628bc228fff26
Real <swrely@qq.com>
2023-01-18 14:50:04 +0800

    feat: add common abstract design

--- rpc-spring-boot-starter/src/main/java/com/company/rpcspringbootstarter/annotation/ServiceProvider.java
+++ rpc-spring-boot-starter/src/main/java/com/company/rpcspringbootstarter/annotation/ServiceProvider.java
@@ -1,8 +1,21 @@
 package com.company.rpcspringbootstarter.annotation;
 
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.*;
+
 /**
+ * 标识服务提供者，暴露服务接口
+ *
  * @author wei.song
  * @since 2023/1/18 14:23
  */
+@Component
+@Documented
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface ServiceProvider {
+
+    String value() default "";
+
 }
--- rpc-spring-boot-starter/src/main/java/com/company/rpcspringbootstarter/common/ServiceInterfaceInfo.java
+++ rpc-spring-boot-starter/src/main/java/com/company/rpcspringbootstarter/common/ServiceInterfaceInfo.java
@@ -1,6 +1,47 @@
-package com.company.rpcspringbootstarter.common;/**
+package com.company.rpcspringbootstarter.common;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * 服务提供者对外暴露的信息
+ * 服务中每个对外暴露的接口都会当成一种服务能力暴露出去，这个服务能力仅仅是接口能力的简单抽象
+ *
  * @author wei.song
  * @since 2023/1/18 14:39
  */
+@Data
+@Builder
 public class ServiceInterfaceInfo {
+
+    /**
+     * 服务名（接口全限定名）
+     */
+    private String serviceName;
+
+    /**
+     * 实例 id，每个服务实例不一样
+     */
+    private String instanceId;
+
+    /**
+     * 服务实例 ip 地址，每个实例不一样
+     */
+    private String ip;
+
+    /**
+     * 服务端口号，每个实例一样
+     */
+    private Integer port;
+
+    /**
+     * 实现该接口 bean 对象对应的class 对象，用于后续反射调用
+     */
+    private Class<?> clazz;
+
+    /**
+     * 实现该接口的 bean 对象，用于后续反射调用
+     */
+    private Object obj;
+
 }
