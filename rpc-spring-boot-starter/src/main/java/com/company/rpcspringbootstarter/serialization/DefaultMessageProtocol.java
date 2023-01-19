f97e2e4c8f9e8d2b4211eb3a1857cb14a2889507
Real <swrely@qq.com>
2023-01-19 09:45:19 +0800

    feat: add java default serialization

--- rpc-spring-boot-starter/src/main/java/com/company/rpcspringbootstarter/serialization/DefaultMessageProtocol.java
+++ rpc-spring-boot-starter/src/main/java/com/company/rpcspringbootstarter/serialization/DefaultMessageProtocol.java
@@ -1,5 +1,7 @@
 package com.company.rpcspringbootstarter.serialization;
 
+import java.io.*;
+
 /**
  * Default serialization protocol: java default serialization
  *
@@ -9,21 +11,48 @@ package com.company.rpcspringbootstarter.serialization;
 public class DefaultMessageProtocol implements MessageProtocol {
     @Override
     public RpcRequest decodeRequestMessage(byte[] data) throws Exception {
-        return null;
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(data);
+        ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+        RpcRequest rpcRequest = (RpcRequest) objectInputStream.readObject();
+        inputStream.close();
+        objectInputStream.close();
+        return rpcRequest;
     }
 
     @Override
     public byte[] encodeRequestMessage(RpcRequest rpcRequest) throws Exception {
-        return new byte[0];
+        return serialize(rpcRequest);
     }
 
     @Override
     public RpcResponse decodeResponseMessage(byte[] data) throws Exception {
-        return null;
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(data);
+        ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+        RpcResponse rpcResponse = (RpcResponse) objectInputStream.readObject();
+        inputStream.close();
+        objectInputStream.close();
+        return rpcResponse;
     }
 
     @Override
     public byte[] encodeResponseMessage(RpcResponse rpcResponse) throws Exception {
-        return new byte[0];
+        return serialize(rpcResponse);
+    }
+
+    /**
+     * 序列化
+     *
+     * @param object 对象
+     * @return {@link byte[]}
+     * @throws IOException IO异常
+     */
+    private byte[] serialize(Object object) throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ObjectOutputStream out = new ObjectOutputStream(byteArrayOutputStream);
+        out.writeObject(object);
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+        out.close();
+        byteArrayOutputStream.close();
+        return bytes;
     }
 }
